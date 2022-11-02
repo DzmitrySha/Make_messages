eaec0112eee2d46359f015e28d1d05abb482b358
DzmitrySha <ravenbydev@gmail.com>
2022-11-02 20:42:13 +0300

    refactor names and updated descriptions

--- make_messages/cli.py
+++ make_messages/cli.py
@@ -1,5 +1,6 @@
 """Parser module - parsing args from user command line"""
 
+import os
 import argparse
 
 
@@ -7,7 +8,13 @@ def parsing_args():
     """Parsing args from user command line"""
     parser = argparse.ArgumentParser(
         prog='make_messages',
-        description="Make messages from special .xlsx file")
+        description="""
+        Make messages from special .xlsx file with table of last names, 
+        pairs of parent and child names and template message in a row. 
+        Returns a list of unique messages with every pair 
+        of parent and child names inside."""
+    )
     parser.add_argument('xlsx_filepath', type=str)
+    parser.add_argument('-o', '--output', default=os.getcwd())
     args = parser.parse_args()
     return args.xlsx_filepath
